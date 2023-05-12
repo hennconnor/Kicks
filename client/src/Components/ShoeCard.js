bf8e1a633f650eae72e4f256bca707c8aa6933a0
Connor Henn <connor.henn@yahoo.com>
2023-05-11 17:23:53 -0700

    fixed props

--- client/src/Components/ShoeCard.js
+++ client/src/Components/ShoeCard.js
@@ -1,11 +1,11 @@
-const ShoeCard = (shoe) => {
+const ShoeCard = ({ shoe }) => {
     return (
-        <div class='card'>
-            <p>{shoe.shoe.name}</p>
-            <img src={shoe.shoe.picture} alt='shoe' height='300' width='300' />
-            <p>Brand: {shoe.shoe.brand}</p>
-            <p>Color: {shoe.shoe.color}</p>
-            <p>Price: {shoe.shoe.price}</p>
+        <div className='card'>
+            <p>{shoe.name}</p>
+            <img src={shoe.picture} alt='shoe' height='300' width='300' />
+            <p>Brand: {shoe.brand}</p>
+            <p>Color: {shoe.color}</p>
+            <p>Price: {shoe.price}</p>
         </div>
     )
 }
